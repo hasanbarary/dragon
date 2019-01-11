4cd2d756e6bad1c6df981cc54504c484487c4243
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-11 23:29:54 +0330

    add x y for X in table

--- dragon.py
+++ dragon.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3.6
-flag=1
+from random import randint
 square=int(input("Enter Side of the square --> "))
 for i in range(0,square):
     print(" _",end="")
 print("")
+x=randint(0,square)
+y=randint(0,square)
 for i in range(0,square):
     for j in range(0,square+1):
         print("|",end="")
-        if i==2 and j==5:
+        if i==x and j==y:
             print("X",end="")
         elif not j == square:
             print("_",end="")
     print("")
+print(x+1,y+1)
