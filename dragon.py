6d52838c8587c9a9fc82869aaa3a5a3dc2fff1b3
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-11 23:17:41 +0330

    add change from input

--- dragon.py
+++ dragon.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3.6
 flag=1
-for i in range(0,10):
+square=int(input("Enter Side of the square --> "))
+for i in range(0,square):
     print(" _",end="")
 print("")
-for i in range(0,10):
-    for j in range(0,11):
+for i in range(0,square):
+    for j in range(0,square+1):
         print("|",end="")
         if i==2 and j==5:
             print("X",end="")
-        elif not j == 10:
+        elif not j == square:
             print("_",end="")
     print("")
