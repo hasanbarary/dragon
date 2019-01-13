70bdc062550573b28a27813487a7ac76f34b13e5
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-13 06:58:59 +0300

    Edit help for user & change move click to w s a d
    w for UP
    s for DOWN
    a for LEFT
    d for RIGHT
    
    q for EXIT

--- dragon.py
+++ dragon.py
@@ -3,6 +3,8 @@ from random import randint
 import os
 import sys
 import tty, termios
+def print_help():
+    print("\n          w:(UP)      \na:(LEFT)         d:(RIGHT)\n         s:(DOWN)\n\nq:EXIT\n\n>>> ")
 def getch():
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
@@ -54,6 +56,7 @@ def left():
         return x
 def rand():
        return  randint(0,square-1)
+os.system('clear')
 square=int(input("Enter Side of the square  >>> "))
 dragonx=rand()
 dragony=rand()
@@ -68,22 +71,24 @@ while True:
         break
     if x==doorx and y==doory:
         print("congratulation,You are the Best")
-        break   
+        break  
+    print_help()
 #    answer=input("Enter your direction \n'r' for right \n'l' for left \n'u' for up \n'd' for down \n --> ").lower()
-    print("Enter your direction \n'r' for right \n'l' for left \n'u' for up \n'd' for down \n'q' for exit  \n >>> ")
+#    print("Enter your direction \n'd' for RIGHT \n'a' for LEFT \n'w' for UP \n's' for DOWN \n'q' for EXIT\n >>> ")
     answer=getch()
-    if answer == 'u' or answer == 'up':
+    if  answer == 'w':
         y=up()
-    elif answer == 'd' or answer == 'down':
+    elif  answer == 's':
         y=down()
-    elif answer == 'r' or answer == 'right':
+    elif  answer == 'd':
         x=right()
-    elif answer == 'l' or answer == 'left':
+    elif  answer == 'a':
         x=left()
     elif answer == 'q':
         break
     else:
         os.system('clear')
-        print("Just Enter 'u' 'd' 'r' 'l'")
+        print("Just Enter UP:'u'or'w'  DOWN:'d'or's' RIGHT:'r'or'd' LEFT:'l'or'a'")
         continue
     os.system('clear')
+
